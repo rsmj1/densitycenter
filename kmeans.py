70cca0041ec0e959edfe616149d46a66e5b43e99
rsmj1 <rsmj98@gmail.com>
2024-04-22 19:18:17 +0200

    Implemented the efficient algorithm for k-means as well. Needs to be briefly tested.

--- kmeans.py
+++ kmeans.py
@@ -216,6 +216,168 @@ class DCKMeans(object):
     dists = np.array([[dc_tree.dc_dist(c,p_index) for c in centers] for p_index in range(n)])
     cluster_assignments = np.argmin(dists, axis=1)
     return cluster_assignments
+  
+
+  def assign_points_eff(self, points, dc_tree):
+    '''
+    Assigns the points bottom up to the provided centers in O(n).
+    It bottom up creates a list of tuples of the point and its center. 
+    Finally, this list is rearranged into the standard format of labelling.
+    It resolves distance ties to the leftmost center in the subtree as an arbitrary choice.
+    '''
+    n = points.shape[0]
+
+    tuple_list = self.label_points_tuples(dc_tree)
+
+    labels = np.zeros(n)
+    for tup in tuple_list:
+       i = tup[0]
+       label = tup[1]
+       labels[i] = label
+    labels = normalize_cluster_ordering(labels) #THIS STEP IS NOT O(n) DUE TO USING SET IN 
+    return labels
+
+
+
+  def label_points_tuples(self, dc_tree):
+     '''
+     This labels the points and outputs a list of tuples of the point_id and the center it is assigned to. 
+     It is done this way to avoid having to use a global list. 
+     '''
+     output = []
+
+     def list_builder(dc_tree, list, center):
+        '''
+        Helper method that does the recursive list building. 
+        It remembers the last center seen on the path from the root to the leaf, which is the center that gets the point assigned. 
+        '''
+        if dc_tree.chosen:
+              center = dc_tree.best_center
+
+        if dc_tree.is_leaf:
+           list.append((dc_tree.point_id, center))
+        else:
+           list_builder(dc_tree.left_tree, list, center)
+           list_builder(dc_tree.right_tree, list, center)
+
+     list_builder(dc_tree, output, -1)
+     return output #List of tuples
+
+  def efficient_greedy(self, points):
+     '''
+     Solves the K-median problem optimally with complexity O(n * log(n)).
+     It does so by greedily choosing the next center as the point that reduces the cost the most. 
+     
+     This is the approach that sorts a list.
+
+     The other approach would use heapq. Issue with this is that we need to re-annotate the tree anyways with the specific choices made in the end (or just set a marker to true).
+     '''
+     n = points.shape[0]
+     placeholder = np.zeros(n)
+     dc_tree, _ = make_tree(points, placeholder, min_points=self.min_pts, )
+     annotations = self.annotate_tree(dc_tree)
+
+     annotations.sort(reverse=True, key=lambda x : x[0]) #Sort by the first value of the tuples - the potential cost-decrease. Reverse=True to get descending order.
+     cluster_centers = set() # We should not use the "in" operation, as it is a worst-case O(n) operation. Just add again and again
+
+     for annotation in annotations:
+        curr_len = len(cluster_centers)
+        if curr_len >= self.k:
+           break
+        cluster_centers.add(annotation[1])
+        new_len = len(cluster_centers)
+        if curr_len != new_len: #This janky setup is to make sure we do not need to use the "in" operation which has a worst-case O(n) complexity
+           annotation[2].chosen = True
+           annotation[2].best_center = annotation[1] 
+     # Now we just need to assign the points to the clusters.
+     self.labels_ = self.assign_points_eff(points, dc_tree)
+     print("labels:", self.labels_)
+     centers = list(cluster_centers)
+     self.center_indexes = centers
+     self.centers = points[centers]
+     return
+  
+  def annotate_tree(self, tree):
+     '''
+     Does bottom-up on the dc-tree, generating an array of the annotations. We do not need to keep track of which tree-node generated which annotation
+     , so the order in which they are inserted into the array does not matter. 
+     Appending to a list is amortized O(n).
+     It follows the cost_computation algorithm from my paper.
+
+     Since this is for K-means, notice that distances are squared whenever used!!
+     '''
+
+     output = []
+     def annotation_builder(tree, array, parent_dist):
+        if tree.is_leaf:
+          array.append((parent_dist**2, tree.point_id, tree)) #Need to append a pointer to the tree itself because we want to mark the spots in the tree form which a point was chosen.
+          #print("Annotation:", parent_dist, tree.point_id,  self.get_leaves(tree))
+          return 0, tree.point_id, 1 #Returns local cost, the center for that cost and the size of the tree
+        else:
+           left_cost, left_center, left_size = annotation_builder(tree.left_tree, array, tree.dist)
+           right_cost, right_center, right_size =  annotation_builder(tree.right_tree, array, tree.dist)
+           total_size = left_size + right_size
+
+           cost_left_center = left_cost + right_size * (tree.dist**2)
+           cost_right_center = right_cost + left_size * (tree.dist**2)
+
+          #We do not need to keep track of whether we are in the root.
+           if cost_left_center > cost_right_center: #C_L > C_R implies higher decrease for C_R
+              cost_decrease = (parent_dist**2) * total_size - cost_right_center 
+              array.append((cost_decrease, right_center, tree))
+              #print("Annotation:", cost_decrease, right_center, self.get_leaves(tree))
+
+              return cost_right_center, right_center, total_size
+           else:
+              cost_decrease = (parent_dist**2) * total_size - cost_left_center 
+              array.append((cost_decrease, left_center, tree))
+              #print("Annotation:", cost_decrease, left_center,  self.get_leaves(tree))
+
+              return cost_left_center, left_center, total_size
+     annotation_builder(tree, output, np.inf)
+     return output
+  
+  def get_leaves(self, dc_tree):
+        '''
+        Returns the set of ids of the leaf nodes within the given cluster.
+        '''
+        if dc_tree.is_leaf:
+            return [dc_tree.point_id]
+        else:
+            #print("left:", self.get_leaves(dc_tree.left_tree))
+            #print("right:", self.get_tree_size(dc_tree.right_tree))
+            return self.get_leaves(dc_tree.left_tree) + self.get_leaves(dc_tree.right_tree)
+  
+
+
+def normalize_cluster_ordering(cluster_labels):
+    '''
+    Normalizes the clustering labels so that the first cluster label encountered is labelled 0, the next 1 and so on. 
+    Preserves noise labelled as -1. Useful for clustering comparisons.
+    '''
+    #print("cluster_labels before:", cluster_labels)
+    n = cluster_labels.shape[0]
+    cluster_index = 0
+    cluster_index_mapping = {}
+    mapped_labels = set()
+    mapped_labels.add(-1)
+    norm_cluster_labels = np.empty(n, dtype=np.int64)
+
+    for i, label in enumerate(cluster_labels):
+        if label not in mapped_labels:
+            #Create mapping for new encountered cluster
+            mapped_labels.add(label)
+            cluster_index_mapping[label] = cluster_index
+            cluster_index += 1
+        
+        if label == -1:
+            #Preserve noise labellings
+            norm_cluster_labels[i] = -1
+        else:
+            norm_cluster_labels[i] = cluster_index_mapping[label]
+
+    #print("cluster_labels after:", norm_cluster_labels)
+    return norm_cluster_labels
 
 
 #Basic testing:
